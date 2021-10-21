39ccd5b86e01b521fe4abec04348e8ce75171412
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-10-21 22:41:56 +0200

    Creacion completa de la fase 3 y cuatro, con las funciones de carrera y cambio de pantalla a fase 4, a falta de darle estilo a la fase del ganador y realizar el readme

--- js/function.js
+++ js/function.js
@@ -23,26 +23,33 @@ const cambiarPantalla = (valor) => {
 // Instanciar los corredores
 
 class Corredores {
-    constructor(nombre, aceleracion, velocidad, imagen) {
+    constructor(nombre, imagencorriendo, imagen) {
         this.nombre = nombre;
-        this.aceleracion = aceleracion;
-        this.velocidad = velocidad;
+        this.distancia = 0;
+        this.imagencorriendo = imagencorriendo;
         this.imagen = imagen;
     }
 
+    setCorrer(potencia) {
+        this.distancia += potencia;
+    }
+    getCorrer(){
+
+    }
 }
 
-let Corredores1 = new Corredores("sonic", "20", "60", "soniccorriendo.gif")
-let Corredores2 = new Corredores("pikachu", "30", "40", "pikachucorriendo.gif")
-let Corredores3 = new Corredores("luffy", "40", "50", "luffy.gif")
-let Corredores4 = new Corredores("itachi", "35", "45", "itachicorriendo.gif")
+let Corredores1 = new Corredores("sonic", "soniccorriendo.gif", "sonicsprite.gif")
+let Corredores2 = new Corredores("pikachu", "pikachucorriendo.gif", "pikachusprite.gif")
+let Corredores3 = new Corredores("luffy", "luffy.gif", "luffysprite.gif")
+let Corredores4 = new Corredores("itachi", "itachicorriendo.gif", "itachisprite.gif")
 
 
 //Variables
 
 let corredor1 = "";
 let corredor2 = "";
-let indice = 0;
+let ganador = "";
+let potencia = 500;
 
 
 // Traductor
@@ -83,57 +90,110 @@ const seleccionarCorredores = (personajeElegido) => {
 }
 
 
-// Distancia que recorren los corredores
-
-let Metroscorredor1 = 0;
-let Metroscorredor2 = 0;
-let Metrosarecorrer = 4000;
-
-
 // Mostrar los corredores seleccionados
 
 const corredoresMostrados = () => {
 
     document.getElementById("carrera").innerHTML =
         `
-        <div class="personajeacorrer1"><img src= "/items/${corredor1.imagen}" alt="">
+        <div class="personajeacorrer1"><img class="pic" src= "/items/${corredor1.imagencorriendo}" alt="">
         </div>
-            <div class="personajeacorrer2"><img src= "/items/${corredor2.imagen}" alt="">
-            </div>
+            <div class="personajeacorrer2"><img class="pic" src= "/items/${corredor2.imagencorriendo}" alt=""></div>
+        <div class="metrosrecorridos">
+            <div  class="metro1" id="potenciacorredor1">${corredor1.distancia}</div>
+            <div  class="metro2" id="potenciacorredor2">${corredor2.distancia}</div>
+        </div>
+        <div class="boton1"  id="cmptcn" onclick="acelerar()"><button>CORRER</button></div> 
             `;
 }
 
 // Funcion de carrera
 
 
-// function checkIndice() {
-//     if (indice == 2) {  
-//         cambiarPantalla("4");
-//         var IntervaloDistancia = window.setInterval(function () {
-//             Metroscorredor1 += Math.random() * (100 - 50) + 50;
-//             Metroscorredor2 += Math.random() * (100 - 50) + 50;
-//             if (Metroscorredor1 >= Metrosarecorrer || Metroscorredor2 >= Metrosarecorrer) {
-//                 document.getElementById("botonResultado").style.display = "block";
-//                 clearInterval(IntervaloDistancia);
-//                 if (Metroscorredor1 < Metroscorredor2) {
-//                     document.getElementById("ganador").src = "img/" + corredor1 + ".jpg";
-//                 }
-//                 else {
-//                     document.getElementById("ganador").src = "img/" + corredor2 + ".jpg";
-//                 }
-//             }   
-//             document.getElementById("contadorcorredor1").innerHTML = "Metros recorridos: " + Metroscorredor1.toFixed(2);
-//             document.getElementById("contadorcorredor2").innerHTML = "Metros recorridos: " + Metroscorredor2.toFixed(2);
-//         }, 1000);
-//     }
-// }
+const acelerar1 = () => {
+    let random = Math.floor(Math.random() * (50 - 5) + 5);
+    corredor1.distancia = parseInt((random + corredor1.distancia))
 
-// Funcion resetear carrera
+    console.log(corredor1.distancia);
+    document.getElementById("cmptcn").style.botton = `${corredor1.distancia}`
+    document.getElementById("potenciacorredor1").innerHTML = `P1:${corredor1.distancia}`;
+}
+const acelerar2 = () => {
 
-// function resetearJuego() {
-//     indice = 0;
-//     Metroscorredor1 = 0;
-//     Metroscorredor2 = 0;
-//     cambiarPantalla("1");
-//     document.getElementById("botonResultado").style.display = "none";
-// }
+    let random = Math.floor(Math.random() * (50 - 5) + 5);
+    corredor2.distancia = parseInt((random + corredor2.distancia))
+
+    console.log(corredor2.distancia);
+    document.getElementById("cmptcn").style.botton = `${corredor2.distancia}`
+    document.getElementById("potenciacorredor2").innerHTML = `P2:${corredor2.distancia}`;
+}
+
+
+const acelerar = () => {
+    acelerar1();
+    acelerar2();
+    compararCorredores();
+}
+
+
+const compararCorredores = () => {
+
+    if (corredor1.distancia < potencia && corredor2.distancia < potencia) {
+
+    console.log("continua la carrera");
+    } else {
+    document.getElementById("cmptcn").style.display = "none";
+    if (corredor1.distancia > corredor2.distancia && corredor2.distancia < corredor1.distancia) {
+        console.log(`THE WINNER IS ${corredor1.nombre}`);
+
+        setTimeout(() => {
+            cambiarPantalla("4");
+            final();
+        }, 1000);
+
+        ganador = corredor1;
+        ganador.innerHTML = `El ganador es ${corredor1.nombre} !!!!`;
+        console.log(ganador)
+
+    } else {
+        console.log(`THE WINNER IS ${corredor2.nombre}`)
+
+        setTimeout(() => {
+            cambiarPantalla("4");
+            final();
+        }, 1000);
+
+        ganador = corredor2;
+        ganador.innerHTML = `El ganador es ${corredor2.nombre} !!!!`;
+        console.log(ganador)
+
+        }
+    }
+}
+
+const final = () => {
+    document.getElementById("winner").innerHTML =
+    `
+    <div class="camp">
+    <h1 class="tituloganador">El ganador es:</h1>
+    <div class="imagenganador"><img src='/items/${ganador.imagen}'/></div>
+    <div class="nombreganador">${ganador.nombre}</div>  
+    </div>
+    <div class="hwinner">
+    <div class="botonrestart">
+            <button id="restart" type="button" onclick=restart()>RESTART</button>
+        </div>
+    </div>
+    </div>
+    `;
+}
+  //funcion restart
+const restart = () => {
+    cambiarPantalla("1");
+    corredor1.distance = 0;
+    corredor2.distance = 0;
+    corredor1.potencia = 0;
+    corredor2.potencia = 0;
+    window.location.reload(true);
+
+}
