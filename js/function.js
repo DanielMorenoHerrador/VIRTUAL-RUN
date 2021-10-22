026a1f5d5e71a435d6f02ee247548b5a41f6005a
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-10-22 02:12:23 +0200

    Cambio de algunas etiquetas y añadir funcion bienvenida al resetear el juego

--- js/function.js
+++ js/function.js
@@ -195,6 +195,7 @@ const final = () => {
 
 
 //funcion restart
+
 const restart = () => {
     cambiarPantalla("1");
     corredor1.distance = 0;
@@ -203,4 +204,41 @@ const restart = () => {
     corredor2.potencia = 0;
     window.location.reload(true);
 
+}
+
+//Funcion recordar nombre
+
+const setCookie = (cname,cvalue,exdays) => {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+const getCookie = (cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+const checkCookie = () => {
+    let user = getCookie("username");
+        if (user != "") {
+        alert("Welcome again " + user);
+        } else {
+            user = prompt("Please enter your name:","");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
 }
